c48342d7bd2e7c5f5e4e3e84c943d178c6068eb2
heydaremir <2110511150@mahasiswa.upnvj.ac.id>
2024-12-14 10:15:16 +0700

    Deskripsi perubahan yang Anda lakukan

--- backend/app.js
+++ backend/app.js
@@ -7,20 +7,15 @@ const emailRoutes = require('./routes/emailRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173', // Alamat frontend
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
-app.use(bodyParser.json()); // Mendukung JSON payload
+app.use(cors()); // Izinkan semua koneksi
+app.use(bodyParser.json()); // Parsing JSON payload
 app.use(express.json()); // Mendukung JSON parsing untuk request body
 
 // Routes
 app.use('/api/email', emailRoutes); // Route untuk email
-app.use('/api', require('./routes/emailRoutes')); // Sesuaikan path jika berbeda
 
 // Server
-const PORT = process.env.PORT || 5000; // Gunakan PORT dari environment variable atau default ke 3000
+const PORT = process.env.PORT || 5000; // Gunakan PORT dari environment variable atau default ke 5000
 app.listen(PORT, () => {
     console.log(`Backend running on http://localhost:${PORT}`);
 });
